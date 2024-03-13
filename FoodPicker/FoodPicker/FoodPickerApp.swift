d0782fe432f4f35a5c4cd4718e72671b9946630d
Jason Liu <jasonliu1990@gmail.com>
2024-03-13 22:04:40 +0800

    feat: add food picker

---
+++ FoodPicker/FoodPicker/FoodPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodPickerApp.swift
+//  FoodPicker
+//
+//  Created by 劉家達 on 2024/2/20.
+//
+
+import SwiftUI
+
+@main
+struct FoodPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
